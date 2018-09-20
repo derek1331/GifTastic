cc040a990942c2d6985dad6eb33bbd7deca74a11
Derek Reed <1331derek@gmail.com>
2018-09-20 17:56:57 -0600

    prettify

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,35 +1,44 @@
-var topics = ["Ouch", "Sorry", "Florida", "Oops", "Great White", "Messi Skills", "Explosion", "Denver", "Hmm", "Wink"];
+var topics = [
+  "Ouch",
+  "Sorry",
+  "Florida",
+  "Oops",
+  "Great White",
+  "Messi Skills",
+  "Explosion",
+  "Denver",
+  "Hmm",
+  "Wink"
+];
 
 var buttons = 10;
 
 function displayGifs() {
-
   var gifSearch = $(this).attr("data-name");
   console.log(gifSearch);
-  var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=EWaEUtoOHx2oq192nJfWzXjbVuiTu3IF&q=" + gifSearch + "&limit=10&offset=0&rating=PG-13&lang=en";
+  var queryURL =
+    "https://api.giphy.com/v1/gifs/search?api_key=EWaEUtoOHx2oq192nJfWzXjbVuiTu3IF&q=" +
+    gifSearch +
+    "&limit=10&offset=0&rating=PG-13&lang=en";
 
   // Creating an AJAX call for the specific gify button being clicked
   $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(function (response) {
-
+  }).then(function(response) {
     // Creating a div to hold the gifs
     var gifDiv = $("<div class='holding'>");
 
     // Storing the response data
     var repo = response.data;
 
-
     for (var i = 0; i < repo.length; i++) {
-
       var rating = repo[i].rating;
       console.log(rating);
 
       // Creating an element to have the rating displayed
       var rate = $("<p>").html("Rating: " + rating);
       rate.attr("class", "rating");
-
 
       // Retrieving the URL for the still gif
       var stillURL = repo[i].images.fixed_height_still.url;
@@ -38,51 +47,44 @@ function displayGifs() {
       var movingURL = repo[i].images.fixed_height.url;
 
       // Creating an element to hold the gif
-      var stillimage = $('<img>').attr("src", stillURL)
+      var stillimage = $("<img>")
+        .attr("src", stillURL)
         .attr("data-gif", movingURL)
         .attr("data-index", i)
         .attr("data-img", stillURL)
         .attr("class", "stillimage");
-
 
       // Appending the gifs
       gifDiv.append(stillimage);
 
       // Adding them to html
       $("#gifsholder").html(gifDiv);
-
     }
-
   });
-
 }
-
 
 // Function for displaying gif data
 function myButtons() {
-
   $("#buttonsholder").empty();
 
   // Looping through the array of gif topics
   for (var i = 0; i < topics.length; i++) {
-
-
     var a = $("<button>");
 
     a.addClass("my-button");
     a.attr("data-name", topics[i]);
     a.text(topics[i]);
     $("#buttonsholder").append(a);
-
   }
 }
 
-// This function handles the search 
-$("#addgif").on("click", function (event) {
+// This function handles the search
+$("#addgif").on("click", function(event) {
   event.preventDefault();
 
-
-  var gifSubmit = $("#gifsearch").val().trim();
+  var gifSubmit = $("#gifsearch")
+    .val()
+    .trim();
 
   if (gifSubmit == "") {
     return;
@@ -91,11 +93,7 @@ $("#addgif").on("click", function (event) {
     $("button:first").remove();
     topics.push(gifSubmit);
     myButtons();
-
-
-
   } else {
-
     topics.push(gifSubmit);
     buttons++;
 
@@ -109,7 +107,7 @@ $(document).on("click", ".my-button", displayGifs);
 myButtons();
 
 // Switching between gif-states
-$(document).on("click", ".stillimage", function () {
+$(document).on("click", ".stillimage", function() {
   var index = $(this).attr("data-index");
   var state = $(this).attr("data-gif");
   var state2 = $(this).attr("data-img");
@@ -117,13 +115,7 @@ $(document).on("click", ".stillimage", function () {
   console.log(state);
   if ($(this).attr("src") == state2) {
     $(this).attr("src", state);
-  }
-  else {
+  } else {
     $(this).attr("src", state2);
-  };
+  }
 });
-
-
-
-
-
